f6f5bff4954751b6de1115623d3b5453c7aa68da
hxlong2024 <hxlong@zufe.edu.cn>
2025-02-26 09:35:23 +0800

    Update Footer.js

--- themes/gitbook/components/Footer.js
+++ themes/gitbook/components/Footer.js
@@ -15,20 +15,20 @@ const Footer = ({ siteInfo }) => {
 
   return (
     <footer className='z-20 border p-2 rounded-lg bg:white dark:border-black dark:bg-hexo-black-gray justify-center text-center w-full text-sm relative'>
-      // <SocialButton />
+      <SocialButton />
 
-      // <div className='flex justify-center'>
-      //   <div>
-      //     <i className='mx-1 animate-pulse fas fa-heart' />{' '}
-      //     <a
-      //       href={siteConfig('LINK')}
-      //       className='underline font-bold text-gray-500 dark:text-gray-300 '>
-      //       {siteConfig('AUTHOR')}
-      //     </a>
-      //     .<br />
-      //   </div>
-      //   © {`${copyrightDate}`}
-      // </div>
+      <div className='flex justify-center'>
+        <div>
+          <i className='mx-1 animate-pulse fas fa-heart' />{' '}
+          <a
+            href={siteConfig('LINK')}
+            className='underline font-bold text-gray-500 dark:text-gray-300 '>
+            {siteConfig('AUTHOR')}
+          </a>
+          .<br />
+        </div>
+        © {`${copyrightDate}`}
+      </div>
 
       {siteConfig('BEI_AN') && (
         <>
