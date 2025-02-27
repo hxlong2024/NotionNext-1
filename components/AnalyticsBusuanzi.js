1187ccf7373a758ea1bd6ff15267d913479f5e73
hxlong2024 <hxlong@zufe.edu.cn>
2025-02-27 16:58:30 +0800

    Update AnalyticsBusuanzi.js

--- components/AnalyticsBusuanzi.js
+++ components/AnalyticsBusuanzi.js
@@ -4,6 +4,13 @@
  */
 export default function AnalyticsBusuanzi() {
   return (
-
+    <div className='flex gap-x-1'>
+      <span className='hidden busuanzi_container_site_pv whitespace-nowrap'>
+        <span className='px-1 busuanzi_value_site_pv'> </span>
+      </span>
+      <span className='hidden busuanzi_container_site_uv whitespace-nowrap'>
+        <span className='px-1 busuanzi_value_site_uv'> </span>
+      </span>
+    </div>
   )
 }
